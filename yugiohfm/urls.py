6ded6e5e09fcb0532d5307697541da2948951395
John Bert <jbertlik@gmail.com>
2019-11-28 14:01:22 -0500

    uncommented whiteNoise module so static files can be seen by Heroku when debug mode is off

--- yugiohfm/urls.py
+++ yugiohfm/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.conf.urls import patterns, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from main import views as main_views
 
