f82f647cb8b6810a367389cb9733543e05be2356
John Bert <jbertlik@gmail.com>
2019-11-28 13:12:08 -0500

    changed homepage view to class view, turned debug on temporaily

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from .views import (
+    HomePageView,
+    
+)
 
 
 urlpatterns = [
-    path('', views.home, name='main-home'),
+    # path('', views.home, name='main-home'),
+    path('', HomePageView.as_view(), name='main-home'),
     path('guide/', views.guide, name='main-guide'),
     path('streams/', views.streams, name='main-streams'),
 ]
